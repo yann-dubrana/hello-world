ac60b561aca2561a62bb7611f1ec3812c9d205fa
yann-dubrana <ydubrana@jllspear.com>
2026-03-31 14:14:52 +0200

    implement first commands with typers

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello world!")
+import typer
 
+
+app = typer.Typer()
+
+@app.command()
+def hello():
+    print("Hello World!")
+
+@app.command()
+def hi(name:str = typer.Option("yann")):
+    print(f"Hello {name}")
+
+@app.command()
+def goodbye():
+    print("Goodbye")
 
 if __name__ == "__main__":
-    main()
+    app()
